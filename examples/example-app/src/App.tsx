d4700259983f73b1ea3b017b10fbd00aadd8a7b6
Ben Durrant <ben.durrant@marketdojo.com>
2024-04-29 14:06:24 +0100

    remove unnecessary things from example

--- examples/example-app/src/App.tsx
+++ examples/example-app/src/App.tsx
@@ -1,8 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { useReducerWithDevtools } from "use-reducer-devtools";
 
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { SetStateAction } from "react";
 
@@ -60,15 +58,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <p>
           <code>reduxCount</code> is {reduxCount.value}
@@ -95,9 +84,6 @@ function App() {
           Increase by 5
         </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
